73edbf54b8f8a89241e54e4f1d91e540d35e7b66
duhan.lvxx <duhan.lvxx@gmail.com>
2011-09-08 06:28:47 +0000

    add progress bar

--- RecSysVisual/src/com/Servlet/FPAAnalyseServlet.java
+++ RecSysVisual/src/com/Servlet/FPAAnalyseServlet.java
@@ -3,7 +3,9 @@ package com.Servlet;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,48 +16,64 @@ import org.tseg.analyse.FPAnalyser;
 
 public class FPAAnalyseServlet extends HttpServlet {
 	public String SEPARATOR = "@@@"; 
+	private  AtomicInteger progress =new AtomicInteger(0);
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 	throws ServletException, IOException {
-		System.out.println("------------------����FPAAnalyseServlet");
-		String inputPath = java.net.URLDecoder.decode(request.getParameter("inputPath"), "UTF-8");
-		String outputPath = java.net.URLDecoder.decode(request.getParameter("outputPath"), "UTF-8");
-		String type = java.net.URLDecoder.decode(request.getParameter("type"), "UTF-8");
-		String Closed = java.net.URLDecoder.decode(request.getParameter("Closed"), "UTF-8");
-		String maxFPLenght = java.net.URLDecoder.decode(request.getParameter("maxFPLenght"), "UTF-8");
-		String MinRatio = java.net.URLDecoder.decode(request.getParameter("MinRatio"), "UTF-8");
-		String DecayRatio = java.net.URLDecoder.decode(request.getParameter("DecayRatio"), "UTF-8");
+		System.out.println("------------------进入FPAAnalyseServlet");
 		
-		
-		String webRootPath=getServletContext().getRealPath("/");
-		System.out.print("webRootPath"+webRootPath);
-		Properties properties = new Properties();
-		InputStream in = new FileInputStream(webRootPath+"config/config.properties");
-		properties.load(in);
-		String fileDir = "";
-		fileDir = properties.getProperty("directory");		
-		System.out.println("fileDir is : "+fileDir);
-		
-		String siteDataPath = fileDir;
-		
-		FPAnalyser fp = new FPAnalyser();
-		String params = siteDataPath+this.SEPARATOR+
-		inputPath+this.SEPARATOR+
-		outputPath+this.SEPARATOR+
-		type+this.SEPARATOR+
-		Closed+this.SEPARATOR+
-		maxFPLenght+this.SEPARATOR+
-		MinRatio+this.SEPARATOR+
-		DecayRatio+this.SEPARATOR;
-		
-		System.out.println(params);
-		fp.readParam(params);
-		
-		try {
-			fp.run();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		String action = java.net.URLDecoder.decode(request.getParameter("action"), "UTF-8");
+		if(action.equals("start")){
+			String inputPath = java.net.URLDecoder.decode(request.getParameter("inputPath"), "UTF-8");
+			String outputPath = java.net.URLDecoder.decode(request.getParameter("outputPath"), "UTF-8");
+			String type = java.net.URLDecoder.decode(request.getParameter("type"), "UTF-8");
+			String Closed = java.net.URLDecoder.decode(request.getParameter("Closed"), "UTF-8");
+			String maxFPLenght = java.net.URLDecoder.decode(request.getParameter("maxFPLenght"), "UTF-8");
+			String MinRatio = java.net.URLDecoder.decode(request.getParameter("MinRatio"), "UTF-8");
+			String DecayRatio = java.net.URLDecoder.decode(request.getParameter("DecayRatio"), "UTF-8");
+			
+			
+			String webRootPath=getServletContext().getRealPath("/");
+			System.out.print("webRootPath"+webRootPath);
+			Properties properties = new Properties();
+			InputStream in = new FileInputStream(webRootPath+"config/config.properties");
+			properties.load(in);
+			String fileDir = "";
+			fileDir = properties.getProperty("directory");		
+			System.out.println("fileDir is : "+fileDir);
+			
+			String siteDataPath = fileDir;
+			
+			FPAnalyser fp = new FPAnalyser();
+			fp.getProgress(progress);
+			String params = siteDataPath+this.SEPARATOR+
+			inputPath+this.SEPARATOR+
+			outputPath+this.SEPARATOR+
+			type+this.SEPARATOR+
+			Closed+this.SEPARATOR+
+			maxFPLenght+this.SEPARATOR+
+			MinRatio+this.SEPARATOR+
+			DecayRatio+this.SEPARATOR;
+			
+			System.out.println(params);
+			fp.readParam(params);
+			
+			try {
+				fp.run();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		else if(action.equals("progress")){
+			System.out.println("get's progress:"+progress);
+			response.setContentType("text/xml;charset=utf-8");
+			response.setCharacterEncoding("utf-8");
+			PrintWriter out = response.getWriter();
+			out.println(progress);
+			out.flush();
+			out.close();
+		}
+		
 		
 		
 		
