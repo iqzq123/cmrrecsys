47576aa70f1c197a137e435af9bf31de0ec65bbd
duhan.lvxx <duhan.lvxx@gmail.com>
2011-09-09 04:32:32 +0000

    add try catch for servlet

--- RecSysVisual/src/com/Servlet/BookPackageServlet.java
+++ RecSysVisual/src/com/Servlet/BookPackageServlet.java
@@ -18,34 +18,43 @@ public class BookPackageServlet extends HttpServlet{
 	private String packageSuffix = "/图书打包文件.xml";
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 	throws ServletException, IOException {
-		System.out.println("-------------------\n进入bookPackageServlet!!!!!!");
-		response.setContentType("text/xml;charset=utf-8");
-		response.setCharacterEncoding("utf-8");
+		try {
+			System.out.println("-------------------\n进入bookPackageServlet!!!!!!");
+			response.setContentType("text/xml;charset=utf-8");
+			response.setCharacterEncoding("utf-8");
+			
+			String tundishPath = java.net.URLDecoder.decode(request.getParameter("tundishPath"), "UTF-8");
+//			String packagePath = java.net.URLDecoder.decode(request.getParameter("packagePath"),"UTF-8");
+			String bookID = java.net.URLDecoder.decode(request.getParameter("bookID"),"UTF-8");
+			int n = Integer.parseInt(java.net.URLDecoder.decode(request.getParameter("n"), "UTF-8"));
+			
+			File temp = new File(tundishPath);
+			String packagePath = temp.getParent()+packageSuffix;
+//			tundishPath = tundishPath + tundishSuffix;
+			BookPackage bookPackage = new BookPackage();
+			
+			bookPackage.setInputPath(tundishPath);
+			bookPackage.setOutputPath(packagePath);
+			bookPackage.setBookID(bookID);
+			bookPackage.setN(n);
+			
+			bookPackage.run();
+			
+			
+			PrintWriter out = response.getWriter();
+			XMLFileReader xmlfr = new XMLFileReader();
+			String str = "";
+			str = xmlfr.readXMLToStr(packagePath);      
+			out.println(str);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("ERROR:BookPackageServlet");
+			System.out.println(e.toString());
+		}
 		
-		String tundishPath = java.net.URLDecoder.decode(request.getParameter("tundishPath"), "UTF-8");
-//		String packagePath = java.net.URLDecoder.decode(request.getParameter("packagePath"),"UTF-8");
-		String bookID = java.net.URLDecoder.decode(request.getParameter("bookID"),"UTF-8");
-		int n = Integer.parseInt(java.net.URLDecoder.decode(request.getParameter("n"), "UTF-8"));
-		
-		File temp = new File(tundishPath);
-		String packagePath = temp.getParent()+packageSuffix;
-//		tundishPath = tundishPath + tundishSuffix;
-		BookPackage bookPackage = new BookPackage();
-		
-		bookPackage.setInputPath(tundishPath);
-		bookPackage.setOutputPath(packagePath);
-		bookPackage.setBookID(bookID);
-		bookPackage.setN(n);
-		
-		bookPackage.run();
-		
-		PrintWriter out = response.getWriter();
-		XMLFileReader xmlfr = new XMLFileReader();
-		String str = "";
-		str = xmlfr.readXMLToStr(packagePath);      
-		out.println(str);
-		out.flush();
-		out.close();
 		
 	}
 
--- RecSysVisual/src/com/Servlet/FPAAnalyseServlet.java
+++ RecSysVisual/src/com/Servlet/FPAAnalyseServlet.java
@@ -15,69 +15,72 @@ import javax.servlet.http.HttpServletResponse;
 import org.tseg.analyse.FPAnalyser;
 
 public class FPAAnalyseServlet extends HttpServlet {
-	public String SEPARATOR = "@@@"; 
-	private  AtomicInteger progress =new AtomicInteger(0);
-	public void doGet(HttpServletRequest request, HttpServletResponse response) 
-	throws ServletException, IOException {
+	public String SEPARATOR = "@@@";
+	private AtomicInteger progress = new AtomicInteger(0);
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("------------------进入FPAAnalyseServlet");
-		
-		String action = java.net.URLDecoder.decode(request.getParameter("action"), "UTF-8");
-		if(action.equals("start")){
-			String inputPath = java.net.URLDecoder.decode(request.getParameter("inputPath"), "UTF-8");
-			String outputPath = java.net.URLDecoder.decode(request.getParameter("outputPath"), "UTF-8");
-			String type = java.net.URLDecoder.decode(request.getParameter("type"), "UTF-8");
-			String Closed = java.net.URLDecoder.decode(request.getParameter("Closed"), "UTF-8");
-			String maxFPLenght = java.net.URLDecoder.decode(request.getParameter("maxFPLenght"), "UTF-8");
-			String MinRatio = java.net.URLDecoder.decode(request.getParameter("MinRatio"), "UTF-8");
-			String DecayRatio = java.net.URLDecoder.decode(request.getParameter("DecayRatio"), "UTF-8");
-			
-			
-			String webRootPath=getServletContext().getRealPath("/");
-			System.out.print("webRootPath"+webRootPath);
-			Properties properties = new Properties();
-			InputStream in = new FileInputStream(webRootPath+"config/config.properties");
-			properties.load(in);
-			String fileDir = "";
-			fileDir = properties.getProperty("directory");		
-			System.out.println("fileDir is : "+fileDir);
-			
-			String siteDataPath = fileDir;
-			
-			FPAnalyser fp = new FPAnalyser();
-			fp.getProgress(progress);
-			String params = siteDataPath+this.SEPARATOR+
-			inputPath+this.SEPARATOR+
-			outputPath+this.SEPARATOR+
-			type+this.SEPARATOR+
-			Closed+this.SEPARATOR+
-			maxFPLenght+this.SEPARATOR+
-			MinRatio+this.SEPARATOR+
-			DecayRatio+this.SEPARATOR;
-			
-			System.out.println(params);
-			fp.readParam(params);
-			
-			try {
+		try {
+			String action = java.net.URLDecoder.decode(request
+					.getParameter("action"), "UTF-8");
+			if (action.equals("start")) {
+				String inputPath = java.net.URLDecoder.decode(request
+						.getParameter("inputPath"), "UTF-8");
+				String outputPath = java.net.URLDecoder.decode(request
+						.getParameter("outputPath"), "UTF-8");
+				String type = java.net.URLDecoder.decode(request
+						.getParameter("type"), "UTF-8");
+				String Closed = java.net.URLDecoder.decode(request
+						.getParameter("Closed"), "UTF-8");
+				String maxFPLenght = java.net.URLDecoder.decode(request
+						.getParameter("maxFPLenght"), "UTF-8");
+				String MinRatio = java.net.URLDecoder.decode(request
+						.getParameter("MinRatio"), "UTF-8");
+				String DecayRatio = java.net.URLDecoder.decode(request
+						.getParameter("DecayRatio"), "UTF-8");
+
+				String webRootPath = getServletContext().getRealPath("/");
+				System.out.print("webRootPath" + webRootPath);
+				Properties properties = new Properties();
+				InputStream in = new FileInputStream(webRootPath
+						+ "config/config.properties");
+				properties.load(in);
+				String fileDir = "";
+				fileDir = properties.getProperty("directory");
+				System.out.println("fileDir is : " + fileDir);
+
+				String siteDataPath = fileDir;
+
+				FPAnalyser fp = new FPAnalyser();
+				fp.getProgress(progress);
+				String params = siteDataPath + this.SEPARATOR + inputPath
+						+ this.SEPARATOR + outputPath + this.SEPARATOR + type
+						+ this.SEPARATOR + Closed + this.SEPARATOR
+						+ maxFPLenght + this.SEPARATOR + MinRatio
+						+ this.SEPARATOR + DecayRatio + this.SEPARATOR;
+
+				System.out.println(params);
+				fp.readParam(params);
+
 				fp.run();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+			} else if (action.equals("progress")) {
+				System.out.println("get's progress:" + progress);
+				response.setContentType("text/xml;charset=utf-8");
+				response.setCharacterEncoding("utf-8");
+				PrintWriter out = response.getWriter();
+				out.println(progress);
+				out.flush();
+				out.close();
 			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("ERROR:FPAAnalyseServlet");
+			System.out.println(e.toString());
 		}
-		else if(action.equals("progress")){
-			System.out.println("get's progress:"+progress);
-			response.setContentType("text/xml;charset=utf-8");
-			response.setCharacterEncoding("utf-8");
-			PrintWriter out = response.getWriter();
-			out.println(progress);
-			out.flush();
-			out.close();
-		}
-		
-		
-		
-		
-		
+
 	}
 
 }
