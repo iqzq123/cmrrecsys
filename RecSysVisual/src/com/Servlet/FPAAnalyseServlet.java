52bb11e9d2cfe83fdf8f6b339627724ba46acb7d
duhan.lvxx <duhan.lvxx@gmail.com>
2011-09-28 13:48:42 +0000

    在界面弹出异常，增加监控

--- RecSysVisual/src/com/Servlet/FPAAnalyseServlet.java
+++ RecSysVisual/src/com/Servlet/FPAAnalyseServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.tseg.Starter;
 import org.tseg.analyse.FPAnalyser;
 
 
@@ -20,11 +22,11 @@ import org.tseg.analyse.FPAnalyser;
 public class FPAAnalyseServlet extends HttpServlet {
 	public String SEPARATOR = "@@@";
 	private Hashtable<String, FPAnalyser> fpTable =new Hashtable<String, FPAnalyser>();	
-	private AtomicInteger progress = new AtomicInteger(0);
+	private Hashtable<String, Integer> preNumTable = new Hashtable<String, Integer>();
+	private Hashtable<String, Integer> countTable = new Hashtable<String, Integer>();
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("------------------进入FPAAnalyseServlet");
 		try {
 			String action = java.net.URLDecoder.decode(request
 					.getParameter("action"), "UTF-8");
@@ -61,6 +63,8 @@ public class FPAAnalyseServlet extends HttpServlet {
 
 				FPAnalyser fp = new FPAnalyser();
 				this.fpTable.put(taskID, fp);
+				this.preNumTable.put(taskID, -2);
+				this.countTable.put(taskID, 1);
 				
 				String params = siteDataPath + this.SEPARATOR + inputPath
 						+ this.SEPARATOR + outputPath + this.SEPARATOR + type
@@ -69,7 +73,7 @@ public class FPAAnalyseServlet extends HttpServlet {
 						+ this.SEPARATOR + DecayRatio + this.SEPARATOR;
 
 				System.out.println(params);
-				fp.getProgress(progress);
+	
 				fp.readParam(params);
 
 				fp.run();
@@ -83,15 +87,62 @@ public class FPAAnalyseServlet extends HttpServlet {
 				System.out.println("progress taskID "+taskID);
 				int currentLine = fp.getCurLineNum();
 				int totalLine = fp.getLineAmount();
+				String exceptionString = fp.getExceptionInfo();
+				System.out.println("exceptionString is:"+exceptionString);
+				if(exceptionString!=null){
+					out.println("ERROR:\n"+exceptionString);
+					out.flush();
+					out.close();
+				}
 				out.println(currentLine+","+totalLine);
 				out.flush();
 				out.close();
+			}
+			else if(action.equals("getTaskInfo")){
+				String taskInfo = "";
+				if(fpTable.isEmpty()){
+					taskInfo = "null";
+				}
+				else{
+					Iterator<String> taskIDs = fpTable.keySet().iterator();
+					while(taskIDs.hasNext()){
+						String taskID = taskIDs.next();
+						FPAnalyser fp = fpTable.get(taskID);
+						String inputPath = fp.getInputPath();
+						String outputPath = fp.getOutputPath();
+						int currentLine = fp.getCurLineNum();
+						int totalLine = fp.getLineAmount();
+						int count = this.countTable.get(taskID);
+						int preNum = this.preNumTable.get(taskID);
+						if(preNum>currentLine){
+							count++;
+							this.countTable.remove(taskID);
+							this.countTable.put(taskID, count);
+						}
+						preNum = currentLine;
+						this.preNumTable.remove(taskID);
+						this.preNumTable.put(taskID, preNum);
+						taskInfo = taskInfo+taskID+","+inputPath+","+outputPath+","+currentLine+","+totalLine+","+count+";";
+					}
+				}
+				response.setContentType("text/xml;charset=utf-8");
+				response.setCharacterEncoding("utf-8");
+				PrintWriter out = response.getWriter();
+				out.println(taskInfo);
+				out.flush();
+				out.close();			
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 			System.out.println("ERROR:FPAAnalyseServlet");
 			System.out.println(e.toString());
+			response.setContentType("text/xml;charset=utf-8");
+			response.setCharacterEncoding("utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("ERROR:\n"+e.toString());
+			out.flush();
+			out.close();
 		}
 
 	}
